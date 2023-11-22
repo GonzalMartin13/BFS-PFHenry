2af018ae68949d9c9f4c41b0939f06f23d38751c
xaviercs78 <javiercarosoa@gmail.com>
2023-11-22 13:02:04 -0500

    handlers users

---
+++ server/src/handlers/handlerEnvios.js
@@ -0,0 +1,55 @@
+
+
+const handlerGetEnvio = async(req, res)=>{
+    const {id} = req.params
+    try {
+        const response = await getEnvio(id);
+        !response ? res.status(400).json({error: `No se encontro el Id: ${id}`})
+        : res.status(200).json(response); 
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+};
+
+
+const handlerPostEnvio = async(req, res)=>{
+    const {
+weight,
+dimensions,
+fragile,
+description,       //PENDIENTE PREGUNTAR SEGUN LO QUE TENGO EN EL CUADERNO
+city,                  // SOLO TENGO ESTO PENDIENTE DE ESTA HOJA 
+province,
+destination,
+name, 
+
+    } = req.body;
+
+    try {
+        const response = await postEnvio();  // aca tambien debe ir la misma info del req.body
+        !response ? res.status(400).json({error: 'Falta informacion'})
+        : res.status(201).json(response);
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+};
+
+
+const handlerDeletEnvio = async(req, res)=>{
+    const {id} = req.params;
+    try {
+        const response = await deletEnvio(id);
+        !response ? res.status(400).json({error: `No se encontro el Id: ${id}`})
+        : res.status(200).json({message: `Se ha eliminado el pedido con el ID: ${id}`}); 
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+
+};
+
+
+module.exports = {
+    handlerDeletEnvio,
+    handlerGetEnvio,
+    handlerPostEnvio
+};
--- server/src/routes/enviosRoutes.js
+++ server/src/routes/enviosRoutes.js
@@ -2,7 +2,7 @@ const {Router} = require("express")
 // IMPORTAR ACA LOS HANDLERS// 
 const envios = Router()
 
-envios.get("/:id", /* HANDLER DE BUSQUEDA DE ENVIO */ ) // req.params
+envios.get("/:id",) // req.params
 envios.post("/", /* HANDLER PARA CREAR UN ENVIO*/) //req.body
 envios.delete("/id", /* HANDLER PARA ELIMINAR EL PEDIDO*/) // req.params
 envios.get("/price", /* HANDLER PRECIO */) 
