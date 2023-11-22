def176089b8c3a99e1503a96f33a7a44ca557b7f
marcelcausone <marcel_causone@hotmail.com>
2023-11-22 12:52:27 -0300

    punto partida develop FRONT

---
+++ Client/src/components/QuoteForm/QuoteForm.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+export default function QuoteForm() {
+  return (
+    <div>
+      crear pedidos
+      <InputGroup className="mb-3">
+        <InputGroup.Text id="basic-addon1">Origen: </InputGroup.Text>
+        <Form.Control
+          placeholder="Origen"
+          aria-label="Username"
+          aria-describedby="basic-addon1"
+        />
+      </InputGroup>
+      <InputGroup className="mb-3">
+        <Form.Control
+          placeholder="Recipient's username"
+          aria-label="Recipient's username"
+          aria-describedby="basic-addon2"
+        />
+        <InputGroup.Text id="basic-addon2">@example.com</InputGroup.Text>
+      </InputGroup>
+      <Form.Label htmlFor="basic-url">Your vanity URL</Form.Label>
+      <InputGroup className="mb-3">
+        <InputGroup.Text id="basic-addon3">
+          https://example.com/users/
+        </InputGroup.Text>
+        <Form.Control id="basic-url" aria-describedby="basic-addon3" />
+      </InputGroup>
+      <InputGroup className="mb-3">
+        <InputGroup.Text>$</InputGroup.Text>
+        <Form.Control aria-label="Amount (to the nearest dollar)" />
+        <InputGroup.Text>.00</InputGroup.Text>
+      </InputGroup>
+      <InputGroup>
+        <InputGroup.Text>With textarea</InputGroup.Text>
+        <Form.Control as="textarea" aria-label="With textarea" />
+      </InputGroup>
+    </div>
+  );
+}
