22c084e267d7b8bcb4d65d5eccf1f1cae9a2d102
gonzalo <gonzalo.m.martin@gmail.com>
2023-11-23 11:56:24 -0300

    Ruta de cotizacion de paquetes

--- server/src/handlers/enviospost.js
+++ server/src/handlers/enviospost.js
@@ -1,9 +1,10 @@
+const contolerPrecio = require("../controllers/controlerPrecio")
 const cotizarHandler = async(req, res) =>{
     const {origen, destino, largo, ancho, alto, peso, servicios} = req.body
     const volumen = (ancho * largo * alto);
     try{
-        const precioFinal = 500
-        res.status(200).json({precioFinal})
+        const precioFinal = await contolerPrecio(origen, destino, volumen, peso, servicios)
+        res.status(200).json(precioFinal)
     } catch (error){
         return console.log(error.message)
     }
@@ -11,3 +12,7 @@ const cotizarHandler = async(req, res) =>{
 }
 
 module.exports = cotizarHandler
+
+
+// ruta: http://localhost:3001/envios/ 
+// tipo: POST
