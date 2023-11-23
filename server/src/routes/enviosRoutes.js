dde5c6baa523985ae1f8cabbae5404c7842bc61b
gonzalo <gonzalo.m.martin@gmail.com>
2023-11-23 15:11:02 -0300

    Json Correjido

--- server/src/routes/enviosRoutes.js
+++ server/src/routes/enviosRoutes.js
@@ -8,8 +8,7 @@ const envios = Router()
 
 envios.get("/:id", /* HANDLER DE BUSQUEDA DE ENVIO */ ) // req.params
 envios.post("/", cotizarHandler) //req.body
-envios.delete("/id", /* HANDLER PARA ELIMINAR EL PEDIDO*/) // req.params
-=======
+envios.delete("/id", /* HANDLER PARA ELIMINAR EL PEDIDO*/) // req.param
 envios.get("/:id",handlerGetEnvio) // req.params
 envios.post("/", /* HANDLER PARA CREAR UN ENVIO*/) //req.body
 //envios.delete("/id", /* HANDLER PARA ELIMINAR EL PEDIDO*/) // req.params
