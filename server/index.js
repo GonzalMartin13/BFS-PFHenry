af36a0c61a944588ef6eb7f22a3692dba9e400d0
Abel Leiva <a.ignacio.leivaleiva@gmail.com>
2023-11-24 13:37:10 -0300

    correcion index del server

--- server/index.js
+++ server/index.js
@@ -2,18 +2,8 @@ const server = require("./src/server.js");
 const { conn } = require("./src/db.js");
 const port = process.env.PORT || 3001;
 
-conn
-.sync({ force: true })
-.then(() => {
-	server.listen(3001, () => {
-		console.log(`server conectado a base de datos, puerto ojete ${port}`);
-	});
-})
-.catch((error) => console.error(error));
-  .sync({ force: false })
-  .then(() => {
-    server.listen(3001, () => {
-      console.log(`server conectado a base de datos, puerto ojete ${port}`);
-    });
-  })
-  .catch((error) => console.error(error));
+conn.sync({ force: true }).then(() => {
+  server.listen(3001, () => {
+    console.log(`server conectado a base de datos, puerto ojete ${port}`);
+  });
+});
