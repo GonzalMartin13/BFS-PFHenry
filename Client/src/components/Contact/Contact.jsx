73965f2b70c4de0bafac5156152671f1681939d4
Samayoa <fsamayoal@gmail.com>
2023-11-27 16:56:14 -0600

    Validaciones del formulario

--- Client/src/components/Contact/Contact.jsx
+++ Client/src/components/Contact/Contact.jsx
@@ -1,19 +1,50 @@
 import { useState } from "react";
 import emailjs from "@emailjs/browser";
-import style from "./Contact.module.css";
 import Image from "react-bootstrap/Image";
 import contacto from "../../assets/contacto.png";
+import {
+  validateName,
+  validateMail,
+  validateMessage,
+} from "./validationContact";
 
 const Email = () => {
   const [name, setName] = useState("");
-  const [lastname, setLastname] = useState("");
+  const [phone, setPhone] = useState("12345678");
   const [asunto, setAsunto] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
 
+  const [nameError, setNameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [messageError, setMessageError] = useState("");
+
+  const validateNameOnChange = (value) => {
+    const error = validateName(value);
+    setNameError(error);
+  };
+
+  const validateEmailOnChange = (value) => {
+    const error = validateMail(value);
+    setEmailError(error);
+  };
+
+  const validateMessageOnChange = (value) => {
+    const error = validateMessage(value);
+    setMessageError(error);
+  };
+   
+  
+
   const sendEmail = (event) => {
     event.preventDefault();
+  
+    if (nameError || emailError || messageError) {
+      console.log("Formulario inválido");
+      return;
+    }
 
+      
     emailjs
       .sendForm(
         "service_ko3ekug",
@@ -21,50 +52,67 @@ const Email = () => {
         event.target,
         "Z07yDoQAt6am4G7K6"
       )
-      .then((response) => console.log(response));
-    setName("");
-    setLastname("");
-    setAsunto("");
-    setEmail("");
-    setMessage("").catch((error) => console.log(error));
+      .then((response) => {
+        console.log(response);
+        setName("");
+        setPhone("");
+        setAsunto("");
+        setEmail("");
+        setMessage("");
+      })
+      .catch((error) => console.log(error));
   };
 
   return (
     <div>
       <Image src={contacto} fluid style={{ width: "100%", height: "300px" }} />
-      <div className={style.form}>
-        <form className={style.form_mail} onSubmit={sendEmail}>
-          <h3 className="">
+      <div>
+        <form onSubmit={sendEmail}>
+          <h3>Contactanos</h3>
+          <h6>
             Tienes dudas, comentarios, recomendaciones envianos un mensaje.{" "}
-          </h3>
-          <h1 className={style.title_form}>Contactanos</h1>
+          </h6>
+          <br></br>
           <label>Nombre</label>
+          <br></br>
           <input
             type="text"
             name="user_name"
             value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          {/* <label>Apellido</label>
-        <input type="text"
-        name="lastname"
-        value={lastname}
-        onChange={(e) => setLastname(e.target.value)} /> */}
+            onChange={(e) => {
+              setName(e.target.value);
+              validateNameOnChange(e.target.value);
+            }}
+          /><br></br>
+           <span style={{ color: "red" }}>{nameError}</span>
+        
+          <br></br>
           <label>Email</label>
+          <br></br>
           <input
             type="email"
             name="user_email"
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+            onChange={(e) => {
+              setEmail(e.target.value);
+              validateEmailOnChange(e.target.value);
+            }}
+          /><br></br>
+           <span style={{ color: "red" }}>{emailError}</span>
+          <br></br>
           <label>Mensaje</label>
+          <br></br>
           <textarea
             name="user_message"
             cols="30"
             value={message}
             rows="10"
-            onChange={(e) => setMessage(e.target.value)}
-          />
+            onChange={(e) => {
+              setMessage(e.target.value);
+              validateMessageOnChange(e.target.value);
+            }}
+          /><br></br>
+           <span style={{ color: "red" }}>{messageError}</span>
           <hr />
           <button>Send</button>
         </form>
@@ -74,3 +122,26 @@ const Email = () => {
 };
 
 export default Email;
+
+// action
+// export const sendMessage = async (Data) => {
+//     try {
+//       const response = await axios.post('ruta', Data);
+//       return response.data;
+//     } catch (error) {
+//       throw error;
+//     }
+//   };
+
+// handle
+// const handleSubmit = async (event) => {
+//     event.preventDefault();
+//     if (FormValid()) {
+//       try {
+//         const response = await sendMessage(formData)
+
+//       } catch (error) {
+
+//       }
+//     }
+//   };
