4a2decbe18b0f469da8c7d2a8e0929eefa527e6d
gonzalo <gonzalo.m.martin@gmail.com>
2023-11-28 01:16:31 -0300

    bases ok

--- server/src/routes/enviosRoutes.js
+++ server/src/routes/enviosRoutes.js
@@ -1,11 +1,11 @@
-const {handlerGetEnvioXID, handlerPostEnvio} = require('../handlers/handlerEnvios');
+const {handlerGetEnvioXID, handlerPostEnvio, handlerGetEnvio} = require('../handlers/handlerEnvios');
 const {Router} = require("express")
 const cotizarHandler = require("../handlers/enviospost")
 const envios = Router()
 
 envios.post("/price", cotizarHandler) 
 envios.post("/", handlerPostEnvio) //req.body
-envios.get("/", /* HANDLER DE BUSQUEDA DE TODOS LOS ENVIOS Y POR QUERY */ ) // req.query
+envios.get("/", handlerGetEnvio ) // req.query
 envios.get("/:id",handlerGetEnvioXID) // req.params
 envios.delete("/id", /* HANDLER PARA ELIMINAR EL PEDIDO*/) // req.param
 
