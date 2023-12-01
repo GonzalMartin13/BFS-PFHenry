1f723d0b10dcf1ff04e7069f6d58302ca53d7c2d
Lumari-suma <luz.rivase@gmail.com>
2023-12-01 17:30:32 -0300

    Nuevo controllers, handlers y rutas pos y get para admin

--- server/src/handlers/getadminHandlers.js
+++ server/src/handlers/getadminHandlers.js
@@ -3,10 +3,10 @@ const { getAllAdmin } = require("../controllers/adminControllers");
 
 const getAllAdminsHandler = async (req, res) => {
   try {
-    const admins = await getAllAdmin();
-    res.status(200).json(admins);
-  } catch (error) {
-    res.status(400).send({ error: error.message });
+      const admins = await getAllAdmin();
+      res.status(200).json(admins);
+    }catch (error) {
+     res.status(400).send({ error: error.message });
   }
 };
 
