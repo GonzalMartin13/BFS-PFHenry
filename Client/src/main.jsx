6cadd3ef91c5e82c7da5182c5b598f1751aeaf33
Abel Leiva <a.ignacio.leivaleiva@gmail.com>
2023-12-01 12:14:16 -0300

    agrego el persistgate al main.js

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,13 +1,15 @@
-// index.js
-
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+
 import { BrowserRouter } from "react-router-dom";
+
+import { store, persistor } from "./redux/store/store.js";
 import { Provider } from "react-redux";
 import { Auth0Provider } from "@auth0/auth0-react";
-import { store } from "./redux/store/store";
+import { PersistGate } from "redux-persist/integration/react";
 import "bootstrap/dist/css/bootstrap.min.css";
+
 const domain = import.meta.env.VITE_REACT_APP_AUTH0_DOMAIN;
 const clientId = import.meta.env.VITE_REACT_APP_AUTH0_CLIENT_ID;
 
@@ -20,7 +22,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           clientId={clientId}
           redirectUri={window.location.origin}
         >
-          <App />
+          <PersistGate loading={null} persistor={persistor}>
+            <App />
+          </PersistGate>
         </Auth0Provider>
       </BrowserRouter>
     </Provider>
