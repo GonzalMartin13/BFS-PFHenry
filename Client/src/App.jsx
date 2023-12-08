74e6cc9123bacf682fbb2331203db5a8ef344a73
marcelcausone <marcel_causone@hotmail.com>
2023-12-07 21:45:50 -0300

    aaaa

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 /* eslint-disable no-unused-vars */
 import { Route, Routes, useLocation } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
 import NavBar from "./components/NavBar/NavBar";
 import Home from "./components/Home/Home";
 import About from "./views/About/About";
@@ -19,6 +20,7 @@ import Email from "./components/Contact/Contact";
 import MisEnvios from "./components/misEnvios/misEnvios";
 
 import "./App.css";
+
 import FormEnvio from "./components/FormEnvio/FormEnvio";
 import ErrorPage from "./views/ErrorPage/errorpage";
 import Reviews from "./components/Reviews/reviews";
@@ -26,7 +28,17 @@ import ShowReviews from "./views/showReviews/showreviews"
 
 function App() {
   const location = useLocation();
-  const { admin } = useSelector((state) => state.user);
+  const { isLoggedIn, admin, name, lastName, phone, address } = useSelector(
+    (state) => state.user
+  );
+
+  const { origen, destino, servicios, total } = useSelector(
+    (state) => state.quoter
+  );
+  const envio = useSelector((state) => state.shipping);
+  const todosVacios = Object.values(envio).every(
+    (valor) => valor === "" || (Array.isArray(valor) && valor.length === 0)
+  );
   const [user, setUser] = useState({
     email: "",
     password: "",
@@ -72,22 +84,64 @@ function App() {
         <Route path="/payment" element={<About />} />
         <Route path="/servicios" element={<CardContainer />} />
         <Route path="/sucursales" element={<Mapa />} />
-        <Route path="/envios" element={<MisEnvios />} />
-        <Route path="/profile" element={<Profile />} />
         <Route path="/guia" element={<Pdf />} />
-        <Route path="/confirmacion" element={<FormEnvio />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<ErrorPage />} />
+        //protege ruta /factura redirige a "/" si datos de compra estan vacios
+        <Route
+          element={<ProtectedRoute isAllowed={isLoggedIn && !todosVacios} />}
+        >
+          <Route path="/factura" element={<Comprobante />} />
+        </Route>
+        //Redirige a "/profile" para obligar al usuario a completar datos de
+        perfil
+        <Route
+          element={
+            <ProtectedRoute
+              isAllowed={isLoggedIn && name && lastName & address && phone}
+              redirectTo={"/profile"}
+            />
+          }
+        >
+          <Route path="/envios" element={<MisEnvios />} />
+          //si estado quote esta vacio no permite ingresar a ruta /confirmacion,
+          quizas haya que modificar todo esto...
+        </Route>
+        {isLoggedIn ? (
+          <Route
+            element={
+              <ProtectedRoute
+                isAllowed={
+                  name &&
+                  lastName & address &&
+                  phone &&
+                  origen != "" &&
+                  destino != "" &&
+                  total != "" &&
+                  servicios.length != 0
+                }
+                redirectTo="/profile"
+              />
+            }
+          >
+            <Route path="/confirmacion" element={<FormEnvio />} />
+          </Route>
+        ) : (
+          <Route
+            path="/confirmacion"
+            element={<ProtectedRoute isAllowed={false} />}
+          />
+        )}
+        //verifica que admin.email y isLoggedIn sean true para ir a ruta
+        "/dashboard", caso que de false redirige a "/"
         <Route
           path="/dashboard"
           element={
-            admin.emailAdmin && (
+            <ProtectedRoute isAllowed={isLoggedIn && admin.emailAdmin}>
               <Dashboard updateContextUser={updateContextUser} />
-            )
+            </ProtectedRoute>
           }
         />
-        <Route path="/factura" element={<Comprobante />} />
-        <Route path="/reviews" element={<Reviews />} />
-        <Route path="*" element={<ErrorPage />} />
-        <Route path="/show" element={<ShowReviews />} />
       </Routes>
 <Footer/>
      
