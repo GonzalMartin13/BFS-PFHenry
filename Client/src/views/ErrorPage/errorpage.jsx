ed45e9314bda1efdfcf402acd22600baaa47cb6a
gonzalo <gonzalo.m.martin@gmail.com>
2023-12-08 20:18:37 -0300

    Error condicionado

--- Client/src/views/ErrorPage/errorpage.jsx
+++ Client/src/views/ErrorPage/errorpage.jsx
@@ -1,14 +1,28 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./error.css"
  // Reemplaza con la ruta correcta de tu imagen
 
 const ErrorPage = () => {
+  const query = useLocation()
+  console.log(query.search)
+  let hayquery = false
+  if (query.search){
+      hayquery = true
+  } 
+
   return (
     <div >
       <img src="../../../src/assets/conectores-hembra-vector-desenchufados-diseno-minimalista-404-bichos-sobre-fondo-blanco-tema-electrico-concepto-de-banner-web-corte-de-electricidad-perdida-de-conexion-vector-2ddpt5j.png" alt="error" />
       <h2>Error 404</h2>
-      <h4>No pudimos encontrar la pagina que buscas,</h4>
-      <h4>pero quedate tranquilo, tu envio esta en camino</h4>
+      {hayquery ? (
+        <h4>Hubo un problema en tu pago de mercadopago</h4>
+
+      ) : (
+        <>
+          <h4>No pudimos encontrar la página que buscas,</h4>
+          <h4>pero quédate tranquilo, tu envío está en camino</h4>
+        </>
+      )}
       <p>Aqui tiene algunos enlaces que capaz te sean de gran ayuda </p>
     <ul>
       <li><Link className="li" to="/">Pagina Principal</Link></li>
