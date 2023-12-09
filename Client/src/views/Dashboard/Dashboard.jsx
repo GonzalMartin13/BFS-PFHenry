ee547237441a6b16862c52b1bbce3a924e95c967
Lumari-suma <luz.rivase@gmail.com>
2023-12-09 17:44:56 -0300

    Actualizando Dashboard

--- Client/src/views/Dashboard/Dashboard.jsx
+++ Client/src/views/Dashboard/Dashboard.jsx
@@ -14,7 +14,7 @@ import Swal from "sweetalert2";
 import updateEnvio from "../../utils/updateEnvio";
 
 const Dashboard = ({ updateContextUser }) => {
-  const [setAdminGraphs] = useState([]);
+  const [adminGraphs, setAdminGraphs] = useState([]);
   const [users, setUsers] = useState([]);
   const [envio, setEnvio] = useState([]);
   const [payments, setPayments] = useState([]);
