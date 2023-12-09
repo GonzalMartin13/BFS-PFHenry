680e8d5e2b5966dfbbef4e945af562737f8ec369
gonzalo <gonzalo.m.martin@gmail.com>
2023-12-09 17:01:37 -0300

    cambios deployables

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,6 @@
 //import React from "react";
 /* eslint-disable no-unused-vars */
-import { Route, Routes, useLocation } from "react-router-dom";
+/* import { Route, Routes, useLocation } from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
 import NavBar from "./components/NavBar/NavBar";
 import Home from "./components/Home/Home";
@@ -85,17 +85,17 @@ function App() {
         <Route path="/sucursales" element={<Mapa />} />
         <Route path="/guia" element={<Pdf />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="*" element={<ErrorPage />} />
+        <Route path="*" element={<ErrorPage />} /> */
         {/* //protege ruta /factura redirige a "/" si datos de compra estan vacios */}
-        <Route
+{/*         <Route
           element={<ProtectedRoute isAllowed={isLoggedIn && !todosVacios} />}
         >
           <Route path="/factura" element={<Comprobante />} />
-        </Route>
+        </Route> */}
 
         {/* //Redirige a "/profile" para obligar al usuario a completar datos de
         perfil */}
-        <Route
+{/*         <Route
           element={
             <ProtectedRoute
               isAllowed={isLoggedIn && name && lastName & address && phone}
@@ -103,11 +103,11 @@ function App() {
             />
           }
         >
-          <Route path="/envios" element={<MisEnvios />} />
+          <Route path="/envios" element={<MisEnvios />} /> */}
 
          {/*  //si estado quote esta vacio no permite ingresar a ruta /confirmacion,
           quizas haya que modificar todo esto... */}
-        </Route>
+{/*         </Route>
         {isLoggedIn ? (
           <Route
             element={
@@ -132,10 +132,10 @@ function App() {
             path="/confirmacion"
             element={<ProtectedRoute isAllowed={false} />}
           />
-        )}
+        )} */}
        {/*  //verifica que admin.email y isLoggedIn sean true para ir a ruta
         "/dashboard", caso que de false redirige a "/" */}
-        <Route
+ {/*        <Route
           path="/dashboard"
           element={
             <ProtectedRoute isAllowed={isLoggedIn && admin.emailAdmin}>
@@ -150,7 +150,7 @@ function App() {
   );
 }
 
-export default App;
+export default App; */}
 
 //-------------------- ❌ ❌ No borrar lo de abajo❌❌-------------------------
 //----usar en caso de tener que trabajar con componentes de rutas protegidas 👍
@@ -162,73 +162,68 @@ export default App;
 //----usar en caso de tener que trabajar con componentes de rutas protegidas 👍
 
 ///////////////////////////////////////////////////////////////////////////////////////
-// import { Route, Routes, useLocation } from "react-router-dom";
+ import { Route, Routes, useLocation } from "react-router-dom";
 
-// import NavBar from "./components/NavBar/NavBar";
-// import Home from "./components/Home/Home";
-// import About from "./views/About/About";
-// import Dashboard from "./views/Dashboard/Dashboard";
-// import Footer from "./components/Footer/Footer";
-// import CardContainer from "./components/CardContainer/CardContainer";
-// import Profile from "./views/Profile/Profile";
-// import QuoteForm from "./components/QuoteForm/QuoteForm";
-// import Comprobante from "./components/Compra/Comprobante";
-// import Mapa from "./components/Mapa/Mapa";
-// import Pdf from "./components/Pdf/Pdf";
-// import { useState } from "react";
-// import { useSelector } from "react-redux";
-
-// import Email from "./components/Contact/Contact";
+ import NavBar from "./components/NavBar/NavBar";
+ import Home from "./components/Home/Home";
+ import About from "./views/About/About";
+ import Dashboard from "./views/Dashboard/Dashboard";
+ import Footer from "./components/Footer/Footer";
+ import CardContainer from "./components/CardContainer/CardContainer";
+ import Profile from "./views/Profile/Profile";
+ import QuoteForm from "./components/QuoteForm/QuoteForm";
+ import Comprobante from "./components/Compra/Comprobante";
+ import Mapa from "./components/Mapa/Mapa";
+ import Pdf from "./components/Pdf/Pdf";
+ import { useState } from "react";
+ import { useSelector } from "react-redux";
+ import MisEnvios from "./components/misEnvios/misEnvios";
+ import Email from "./components/Contact/Contact";
 // import MisEnvios from "./components/misEnvios/misEnvios";
+ import "./App.css";
+ /* import Compra from "./components/Compra/Compra";
+ import ComprobantePDF from "./components/Compra/ComprobantePDF"; */
+ import FormEnvio from "./components/FormEnvio/FormEnvio";
+ function App() {
+   const location = useLocation();
+   const { admin } = useSelector((state) => state.user);
+   const [user, setUser] = useState({
+     email: "",
+     password: "",
+     isNew: null,
+     enabled: false,
+   });
+   const updateContextUser = (newUser) => {
+   setUser(newUser);
+ };
+ return (
+   <>
+     {location.pathname !== "/register" && <NavBar />}
+     <Routes>
+       <Route exact path="/" element={<Home />} />
+       <Route path="/cotizacion" element={<QuoteForm />} />
+       <Route path="/about" element={<About />} />
+       <Route path="/contacto" element={<Email />} />
+       <Route path="/payment" element={<About />} />
+       <Route path="/servicios" element={<CardContainer />} />
+       <Route path="/sucursales" element={<Mapa />} />
+       <Route path="/envios" element={<MisEnvios />} />
+       <Route path="/profile" element={<Profile />} />
+       <Route path="/guia" element={<Pdf />} />
+       <Route path="/confirmacion" element={<FormEnvio />} />
+       <Route
+           path="/dashboard"
+           element={
+             admin.emailAdmin && (
+               <Dashboard updateContextUser={updateContextUser} />
+             )
+           }
+         />
+         <Route path="/factura" element={<Comprobante />} />
+       </Routes>
+       <Footer />
+     </>
+   );
+ }
 
-// import "./App.css";
-// /* import Compra from "./components/Compra/Compra";
-// import ComprobantePDF from "./components/Compra/ComprobantePDF"; */
-// import FormEnvio from "./components/FormEnvio/FormEnvio";
-
-// function App() {
-//   const location = useLocation();
-//   const { admin } = useSelector((state) => state.user);
-//   const [user, setUser] = useState({
-//     email: "",
-//     password: "",
-//     isNew: null,
-//     enabled: false,
-//   });
-
-//   const updateContextUser = (newUser) => {
-//     setUser(newUser);
-//   };
-
-//   return (
-//     <>
-//       {location.pathname !== "/register" && <NavBar />}
-
-//       <Routes>
-//         <Route exact path="/" element={<Home />} />
-//         <Route path="/cotizacion" element={<QuoteForm />} />
-//         <Route path="/about" element={<About />} />
-//         <Route path="/contacto" element={<Email />} />
-//         <Route path="/payment" element={<About />} />
-//         <Route path="/servicios" element={<CardContainer />} />
-//         <Route path="/sucursales" element={<Mapa />} />
-//         <Route path="/envios" element={<MisEnvios />} />
-//         <Route path="/profile" element={<Profile />} />
-//         <Route path="/guia" element={<Pdf />} />
-//         <Route path="/confirmacion" element={<FormEnvio />} />
-//         <Route
-//           path="/dashboard"
-//           element={
-//             admin.emailAdmin && (
-//               <Dashboard updateContextUser={updateContextUser} />
-//             )
-//           }
-//         />
-//         <Route path="/factura" element={<Comprobante />} />
-//       </Routes>
-//       <Footer />
-//     </>
-//   );
-// }
-
-// export default App;
+ export default App;
