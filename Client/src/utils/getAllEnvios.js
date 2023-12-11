4d56e09fa21cfb451deb37b022d32c7c39199a75
Lumari-suma <luz.rivase@gmail.com>
2023-12-10 22:30:02 -0300

    Filtro Dashboard

--- Client/src/utils/getAllEnvios.js
+++ Client/src/utils/getAllEnvios.js
@@ -1,22 +1,3 @@
-// import axios from "axios";
-
-// export const getAllEnvios = async () => {
-// 		try {
-// 		const response = await axios.get("http://localhost:3001/envios");
-// 		// const response = await axios.get("https://bfs-pfhenry-production.up.railway.app/envios/");
-
-// 		if (response.status !== 200) {
-// 			throw new Error(`HTTP error! Status: ${response.status}`);
-// 		}
-
-// 		const data = response.data;
-
-// 		return data;
-// 	} catch (error) {
-// 		console.error("Error fetching data:", error);
-// 		throw error;
-// 	}
-// };
 import axios from "axios";
 
 export const getAllEnvios = async () => {
